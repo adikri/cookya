12f3a7b541aea60713b09e8cdd297fdcbe432dba
aditya <adityavsau@gmail.com>
2026-03-05 16:57:27 +0530

    Initial Commit

---
+++ cookya/cookyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookyaApp.swift
+//  cookya
+//
+//  Created by adi on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct cookyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cookyaTests/cookyaTests.swift
@@ -0,0 +1,16 @@
+//
+//  cookyaTests.swift
+//  cookyaTests
+//
+//  Created by adi on 05/03/26.
+//
+
+import Testing
+
+struct cookyaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
